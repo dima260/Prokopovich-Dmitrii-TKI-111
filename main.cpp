415d28072af947ddad5aaf3162c2968f8d162872
dima260 <91672276+dima260@users.noreply.github.com>
2021-11-24 21:28:10 +0000

    задание 1.3

--- main.cpp
+++ main.cpp
@@ -1,46 +1,29 @@
+#define _USE_MATH_DEFINES // for C++
+
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
 /**
- * \brief Вычисляет площадь прямоугольника.
- * \param length Длина стороны прямоугольника.
- * \param width Ширина стороны прямоугольника.
- * \return Площадь прямоугольника.
+ *   \brief Метод расчёта  энергии нужно затратить, 	чтобы расплавить кусок льда массой m, взятый при 		температуре -10°С.
+ *   \param mass Масса в килограммах (кг).
+ *   \return Значение энергии в Джоулях (Дж).
  */
-double Square(const double length, const double width);
+double GetEnergy(const double mass);
 
-/**
- * \brief Вычисляет периметр прямоугольника.
- * \param length Длина стороны прямоугольника.
- * \param width Ширина стороны прямоугольника.
- * \return Периметр прямоугольника.
- */
-double Perimeter(const double length, const double width);
-
-/**
- * \brief Точка входа в программу.
- * \return Код ошибки (0 - успех).
- */
 int main()
 {
-    cout << "Введите длины сторон прямоугольника\n";
-    double a,b;
-    cin >> a >> b;
+    double mass;
+    cout << "Enter mass\n";
+    cin >> mass;
 
-    const auto square = Square(a, b);
-    cout << "Площадь квадрата равна " << square << "\n";
+    const auto energy = GetEnergy(mass);
 
-    const auto perimeter = Perimeter(a, b);
-    cout << "Периметр прямоугольника " << perimeter << endl;
+    cout << "Energy is " << energy;
+    return 0;
 }
 
-double Square(const double length, const double width)
-{
-    return length * width;
-}
-
-double Perimeter(const double length, const double width)
-{
-    return 2 * (length + width);
+double GetEnergy(const double mass){
+		return 4.2 * pow( 10 , 3 ) * mass * ( 0 - 10 ) + 3.4 * pow( 10 , 5 ) * mass;
 }
