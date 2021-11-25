274153c7bec8ed79e020ce5e7309c57d2cb63838
dima260 <91672276+dima260@users.noreply.github.com>
2021-11-25 09:43:01 +0000

    задание 3.1

--- main.cpp
+++ main.cpp
@@ -1,46 +1,32 @@
+#include <cmath>
 #include <iostream>
+#define _USE_MATH_DEFINES
 
 using namespace std;
 
 /**
- * \brief Вычисляет площадь прямоугольника.
- * \param length Длина стороны прямоугольника.
- * \param width Ширина стороны прямоугольника.
- * \return Площадь прямоугольника.
- */
-double Square(const double length, const double width);
+* \brief Математическая функция, рассчитывающая значение функции y
+* \param const double x - константа, имеющая тип данных с плавающей точкой двойной точности
+* \return Возвращает значение функции y
+**/
+
+double gety(const double x);
 
 /**
- * \brief Вычисляет периметр прямоугольника.
- * \param length Длина стороны прямоугольника.
- * \param width Ширина стороны прямоугольника.
- * \return Периметр прямоугольника.
- */
-double Perimeter(const double length, const double width);
-
-/**
- * \brief Точка входа в программу.
- * \return Код ошибки (0 - успех).
- */
+* \brief Точка входа в программу.
+* \return Возвращает 0 в случае успешного выполнения.
+*/
 int main()
 {
-    cout << "Введите длины сторон прямоугольника\n";
-    double a,b;
-    cin >> a >> b;
-
-    const auto square = Square(a, b);
-    cout << "Площадь квадрата равна " << square << "\n";
-
-    const auto perimeter = Perimeter(a, b);
-    cout << "Периметр прямоугольника " << perimeter << endl;
+    const double x = 2;
+    const auto y = gety(x);
 }
 
-double Square(const double length, const double width)
+double gety(const double x)
 {
-    return length * width;
-}
-
-double Perimeter(const double length, const double width)
-{
-    return 2 * (length + width);
+    for (double x = 1; x <= 2; x = x + 0.1)
+    {
+        cout << "y = " << cos(x)-pow(M_E,(-x*x)/2)+x-1 << endl;
+    }
+    return 1;
 }
